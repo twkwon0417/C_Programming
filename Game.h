76c9ac2a72f3c55c05bf0ed2ee91408ae5c9aa42
twkwon0417 <91003152+twkwon0417@users.noreply.github.com>
2024-05-21 16:28:44 +0900

    Update Game.h

--- Game.h
+++ Game.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
+#include <stdlib.h>
 #define MAP_SIZE_W 60
 #define MAP_SIZE_H 28
 #define UP 72
@@ -30,16 +31,18 @@
 #define FRAME 60
 
 void gotoxy(int x, int y);
-int move_key(int (*map)[MAP_SIZE_W], int* x, int* y, int level);
+int move_key(int(*map)[MAP_SIZE_W], int* x, int* y, int level);
 int menu(void);
-void generate_map(int (*map)[MAP_SIZE_W]);
-void level_map(int (*map)[MAP_SIZE_W], int level);
+void generate_map(int(*map)[MAP_SIZE_W]);
+void level_map(int(*map)[MAP_SIZE_W], int level);
 int start_screen(void);
-int flag(int (*map)[MAP_SIZE_W], int* x, int* y, int level);
-void draw_map(int (*map)[MAP_SIZE_W]);
+int flag(int(*map)[MAP_SIZE_W], int* x, int* y, int level);
+void draw_map(int(*map)[MAP_SIZE_W]);
 int game_start(int(*map)[MAP_SIZE_W], int start, int* x, int* y);
 int pause(void);
 void creation_block(int(*map)[MAP_SIZE_W], int level);
 
 void textcolor(int colorNum);
 void article(int level);
+
+void move_boss(int* player_y, int* player_x);

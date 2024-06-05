1aeb730d38813b7122f1d0f6e4a9a42222e2f3f4
twkwon0417 <91003152+twkwon0417@users.noreply.github.com>
2024-06-05 14:30:58 +0900

    Update map.c

--- map.c
+++ map.c
@@ -1,14 +1,10 @@
 #include "Game.h"
 
-int frame_cnt = 0;  // which frame are we in, In 60frames   // could
 int boss_speed_array[] = { 20, 15, 12, 10, 6, 5, 3 };   //*//
 int boss_speed_level = 0;   //*//
-int boss_frame = 20;   //*//
 
 int boss_y = 20;
 int boss_x = 20;
-
-int time_taken = 0;
 
 void move_boss(int(*map)[MAP_SIZE_W], int* player_y, int* player_x) {
     // based on player
@@ -19,53 +15,52 @@ void move_boss(int(*map)[MAP_SIZE_W], int* player_y, int* player_x) {
     int abs_diff_x = abs(diff_x);
 
     // 차이가 크게 나는 쪽을 먼저 움직일꺼야
-    if (map[boss_y + 1][boss_x] == -2 && map[boss_y][boss_x + 1] == -2 && map[boss_y - 1][boss_x] == -2 && map[boss_y][boss_x - 1] == -2)
-    {
-        if (abs_diff_y > abs_diff_x) {
-            if (diff_y > 0) {
-                boss_y++;
-            }
-            else {
-                boss_y--;
-            }
+    if (abs_diff_y > abs_diff_x) {
+        if (diff_y > 0) {
+            boss_y++;
         }
         else {
-            if (diff_x > 0) {
-                boss_x++;
-            }
-            else {
-                boss_x--;
-            }
+            boss_y--;
         }
     }
-    if (map[boss_y + 1][boss_x] == -2)
-    {
-        boss_y++;
-    }
-    if (map[boss_y][boss_x + 1] == -2)
-    {
-        boss_x++;
-    }
-    if (map[boss_y - 1][boss_x] == -2)
-    {
-        boss_y--;
-    }
-    if (map[boss_y][boss_x - 1] == -2)
-    {
-        boss_x--;
+    else {
+        if (diff_x > 0) {
+            boss_x++;
+        }
+        else {
+            boss_x--;
+        }
     }
 }
 void draw_boss(int(*map)[MAP_SIZE_W], int* player_y, int* player_x) {
+    if (map[boss_y][boss_x] != -2) {
+        gotoxy(0, 0);
+        draw_map(map);
+    }
     printf(" \b");
     move_boss(map, player_y, player_x);
     gotoxy(boss_x, boss_y);
     printf("B\b");
+    if (map[boss_y][boss_x] != -2) {
+        gotoxy(0, 0);
+        draw_map(map);
+    }
 }
 
-int check_caught(int* player_y, int* player_x) {   // not yet added to where it supposed to be
-    if (*player_y == boss_y && *player_x == boss_x) {
-        return 0;   // should be defined int meaning game has ended
-    }
+int check_caught(int* y, int* x) {   // not yet added to where it supposed to be
+    if (*x == boss_x && *y == boss_y) {   //*//
+        gotoxy(80, 80);   //*//
+        //printf("hit");   //*//
+        // where deffect happen
+        if (boss_x == 1 && boss_y == 1) {   //*//
+            *x = 60;   //*//
+            *y = 60;   //*//
+        }   //*//
+        else {   //*//
+            *x = 1;   //*//
+            *y = 1;   //*//
+        }   //*//
+    }   //*//
 }
 
 void gotoxy(int x, int y)
@@ -114,22 +109,22 @@ int start_screen(char pn[], int n)
             printf(" \b");
             switch (ch) {
                 //y좌표는 최대 M_H에서 M_H++2
-                case UP: //키보드 UP
-                    if (y > M_H) {
-                        gotoxy(x - 2, y);
-                        printf(" ");
-                        gotoxy(x - 2, --y);
-                        printf(">");
-                    }
-                    break;
-                case DOWN: //키보드 DOWN
-                    if (y < M_H + 2) {
-                        gotoxy(x - 2, y);
-                        printf(" ");
-                        gotoxy(x - 2, ++y);
-                        printf(">");
-                    }
-                    break;
+            case UP: //키보드 UP
+                if (y > M_H) {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, --y);
+                    printf(">");
+                }
+                break;
+            case DOWN: //키보드 DOWN
+                if (y < M_H + 2) {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, ++y);
+                    printf(">");
+                }
+                break;
             }
         }
     } while (1);
@@ -163,22 +158,22 @@ int menu(void)
             printf(" \b");
             switch (ch) {
                 //y좌표는 최대 M_H에서 M_H+3
-                case UP: //키보드 UP
-                    if (y > M_H) {
-                        gotoxy(x - 2, y);
-                        printf(" ");
-                        gotoxy(x - 2, --y);
-                        printf(">");
-                    }
-                    break;
-                case DOWN: //키보드 DOWN
-                    if (y < M_H + 3) {
-                        gotoxy(x - 2, y);
-                        printf(" ");
-                        gotoxy(x - 2, ++y);
-                        printf(">");
-                    }
-                    break;
+            case UP: //키보드 UP
+                if (y > M_H) {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, --y);
+                    printf(">");
+                }
+                break;
+            case DOWN: //키보드 DOWN
+                if (y < M_H + 3) {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, ++y);
+                    printf(">");
+                }
+                break;
             }
         }
     } while (1);
@@ -232,22 +227,22 @@ int rule(void)
             printf(" \b");
             switch (ch) {
                 //y좌표는 최대 M_H에서 M_H+1
-                case UP: //키보드 UP
-                    if (y > M_H + 7) {
-                        gotoxy(x - 2, y);
-                        printf(" ");
-                        gotoxy(x - 2, --y);
-                        printf(">");
-                    }
-                    break;
-                case DOWN: //키보드 DOWN
-                    if (y < M_H + 8) {
-                        gotoxy(x - 2, y);
-                        printf(" ");
-                        gotoxy(x - 2, ++y);
-                        printf(">");
-                    }
-                    break;
+            case UP: //키보드 UP
+                if (y > M_H + 7) {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, --y);
+                    printf(">");
+                }
+                break;
+            case DOWN: //키보드 DOWN
+                if (y < M_H + 8) {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, ++y);
+                    printf(">");
+                }
+                break;
             }
         }
     } while (1);
@@ -291,43 +286,43 @@ void creation_block(int(*map)[MAP_SIZE_W], int level)
         }
         switch (i % 3)
         {
-            case 0:
-                if (map[b_h + 1][b_w] != CLEARSPACE || map[b_h + 2][b_w] != CLEARSPACE || map[b_h + 3][b_w] != CLEARSPACE)
-                {
-                    i--;
-                    break;
-                }
-                map[b_h][b_w] = BLOCK;
-                map[b_h + 1][b_w] = BLOCK;
-                map[b_h + 2][b_w] = BLOCK;
-                map[b_h + 3][b_w] = BLOCK;
+        case 0:
+            if (map[b_h + 1][b_w] != CLEARSPACE || map[b_h + 2][b_w] != CLEARSPACE || map[b_h + 3][b_w] != CLEARSPACE)
+            {
+                i--;
                 break;
-            case 1:
-                if (map[b_h][b_w + 1] != CLEARSPACE || map[b_h][b_w + 2] != CLEARSPACE || map[b_h][b_w + 3] != CLEARSPACE || map[b_h][b_w + 4] != CLEARSPACE || map[b_h][b_w + 5] != CLEARSPACE)
-                {
-                    i--;
-                    break;
-                }
-                map[b_h][b_w] = BLOCK;
-                map[b_h][b_w + 1] = BLOCK;
-                map[b_h][b_w + 2] = BLOCK;
-                map[b_h][b_w + 3] = BLOCK;
-                map[b_h][b_w + 4] = BLOCK;
-                map[b_h][b_w + 5] = BLOCK;
+            }
+            map[b_h][b_w] = BLOCK;
+            map[b_h + 1][b_w] = BLOCK;
+            map[b_h + 2][b_w] = BLOCK;
+            map[b_h + 3][b_w] = BLOCK;
+            break;
+        case 1:
+            if (map[b_h][b_w + 1] != CLEARSPACE || map[b_h][b_w + 2] != CLEARSPACE || map[b_h][b_w + 3] != CLEARSPACE || map[b_h][b_w + 4] != CLEARSPACE || map[b_h][b_w + 5] != CLEARSPACE)
+            {
+                i--;
                 break;
-            case 2:
-                if (map[b_h][b_w + 1] != CLEARSPACE || map[b_h][b_w + 2] != CLEARSPACE || map[b_h + 1][b_w + 3] != CLEARSPACE || map[b_h + 1][b_w + 4] != CLEARSPACE || map[b_h + 1][b_w + 2] != CLEARSPACE)
-                {
-                    i--;
-                    break;
-                }
-                map[b_h][b_w] = BLOCK;
-                map[b_h][b_w + 1] = BLOCK;
-                map[b_h][b_w + 2] = BLOCK;
-                map[b_h + 1][b_w + 2] = BLOCK;
-                map[b_h + 1][b_w + 3] = BLOCK;
-                map[b_h + 1][b_w + 4] = BLOCK;
+            }
+            map[b_h][b_w] = BLOCK;
+            map[b_h][b_w + 1] = BLOCK;
+            map[b_h][b_w + 2] = BLOCK;
+            map[b_h][b_w + 3] = BLOCK;
+            map[b_h][b_w + 4] = BLOCK;
+            map[b_h][b_w + 5] = BLOCK;
+            break;
+        case 2:
+            if (map[b_h][b_w + 1] != CLEARSPACE || map[b_h][b_w + 2] != CLEARSPACE || map[b_h + 1][b_w + 3] != CLEARSPACE || map[b_h + 1][b_w + 4] != CLEARSPACE || map[b_h + 1][b_w + 2] != CLEARSPACE)
+            {
+                i--;
                 break;
+            }
+            map[b_h][b_w] = BLOCK;
+            map[b_h][b_w + 1] = BLOCK;
+            map[b_h][b_w + 2] = BLOCK;
+            map[b_h + 1][b_w + 2] = BLOCK;
+            map[b_h + 1][b_w + 3] = BLOCK;
+            map[b_h + 1][b_w + 4] = BLOCK;
+            break;
         }
     }
 }
@@ -424,6 +419,16 @@ void draw_map(int(*map)[MAP_SIZE_W])
         }
         printf("\n");
     }
+    gotoxy(61, 19);
+    printf("Boss Speed %d", boss_speed_level + 1);
+    gotoxy(61, 20);
+    printf("");
+    for (int i = 0; i < boss_speed_level + 1; i++) {
+        printf("■ ");
+    }
+    if (boss_speed_level == 6) {
+        printf("!!MAX!!");
+    }
 }
 
 int pause(void)
@@ -450,22 +455,22 @@ int pause(void)
             printf(" \b");
             switch (ch) {
                 //y좌표는 최대 M_H에서 M_H+2
-                case UP: //키보드 UP
-                    if (y > M_H) {
-                        gotoxy(x - 2, y);
-                        printf(" ");
-                        gotoxy(x - 2, --y);
-                        printf(">");
-                    }
-                    break;
-                case DOWN: //키보드 DOWN
-                    if (y < M_H + 2) {
-                        gotoxy(x - 2, y);
-                        printf(" ");
-                        gotoxy(x - 2, ++y);
-                        printf(">");
-                    }
-                    break;
+            case UP: //키보드 UP
+                if (y > M_H) {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, --y);
+                    printf(">");
+                }
+                break;
+            case DOWN: //키보드 DOWN
+                if (y < M_H + 2) {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, ++y);
+                    printf(">");
+                }
+                break;
             }
         }
     } while (1);
@@ -474,7 +479,7 @@ int pause(void)
 void first_show(char name[])
 {
     char* str = "계약서(게임 설명)        Enter키를 누르면 skip\n\n\n \\
-    1. 시간 안에 숨겨진 월급을 찾아라.\n\n\n \\
+        1. 시간 안에 숨겨진 월급을 찾아라.\n\n\n \\
         2. 3종류의 미니게임\n   \\
         - 목표 점수 이상 획득 성공 : 월급 10만원 증가\n \\
         - 목표 점수 이상 획득 실패 : 월급 10만원 감소, 점장님 속도 증가\n\n\\
@@ -486,7 +491,7 @@ void first_show(char name[])
         \t\t 받아라.\n\n\\
         확인한 후에 서명하세요.\n\\
         player 서명 : ";
-    slow_print(str, 30);
+        slow_print(str, 30);
     init(1); //커서 안 보이기
     fgets(name, NAMESIZE, stdin);
     name[strcspn(name, "\n")] = '\0';
@@ -497,7 +502,7 @@ void skip()
 {
     system("cls");   //skip후 전체 내용출력
     printf("계약서(게임 설명)\n\n\n\\
-    1. 시간 안에 숨겨진 월급을 찾아라.\n\n\n\\
+        1. 시간 안에 숨겨진 월급을 찾아라.\n\n\n\\
         2. 3종류의 미니게임\n\\
         - 목표 점수 이상 획득 성공 : 월급 10만원 증가\n\\
         - 목표 점수 이상 획득 실패 : 월급 10만원 감소, 점장님 속도 증가\n\n\\
@@ -521,32 +526,6 @@ void skip_f()
 int move_key(int(*map)[MAP_SIZE_W], int* x, int* y, int level, int* money, char playName[])
 {
     Sleep(1000 / FRAME);  // 화면은 1초마다 60번 업데이트 된다.
-
-    frame_cnt++;
-    if (frame_cnt == 61) {   // TODO:not sure if it iterates 60 time and resetted
-        frame_cnt = 1;
-        time_taken++;
-    }
-    // how boss moves
-    if (frame_cnt % boss_frame == 0) {
-        gotoxy(boss_x, boss_y);
-        draw_boss(map, y, x);
-    }
-
-    if (*x == boss_x && *y == boss_y) {   //*//
-        gotoxy(80, 80);   //*//
-        //printf("hit");   //*//
-        // where deffect happen
-        if (boss_x == 1 && boss_y == 1) {   //*//
-            *x = 60;   //*//
-            *y = 60;   //*//
-        }   //*//
-        else {   //*//
-            *x = 1;   //*//
-            *y = 1;   //*//
-        }   //*//
-    }   //*//
-
     int ch, f, stop;
 
     gotoxy(*x, *y);
@@ -563,10 +542,10 @@ int move_key(int(*map)[MAP_SIZE_W], int* x, int* y, int level, int* money, char 
             //이어하기
             if (stop == 0)
                 return stop - 1; //return -1
-                //메인메뉴로
+            //메인메뉴로
             else if (stop == 1)
                 return stop - 3; //return -2
-                //게임종료
+            //게임종료
             else if (stop == 2)
                 return stop - 5; //return -3
         }
@@ -574,70 +553,70 @@ int move_key(int(*map)[MAP_SIZE_W], int* x, int* y, int level, int* money, char 
             ch = _getch();
             printf(" \b");
             switch (ch) {
-                case UP:
-                    if (map[(*y) - 1][*x] == CLEARSPACE)
-                    {
-                        (*y)--;
-                        return GARBAGE;
-                    }
-                    else if (map[(*y) - 1][*x] == WALL || map[(*y) - 1][*x] == BLOCK) //벽
-                        return GARBAGE;
-                    else
-                    {
-                        (*y)--;
-                        f = flag(map, x, y, level, money, playName);
-                        (*y)++;
-                        return f;
-                    }
-                    break;
-                case DOWN:
-                    if (map[(*y) + 1][*x] == CLEARSPACE)
-                    {
-                        (*y)++;
-                        return GARBAGE;
-                    }
-                    else if (map[(*y) + 1][*x] == WALL || map[(*y) + 1][*x] == BLOCK)
-                        return GARBAGE;
-                    else
-                    {
-                        (*y)++;
-                        f = flag(map, x, y, level, money, playName);
-                        (*y)--;
-                        return f;
-                    }
-                    break;
-                case LEFT:
-                    if (map[*y][(*x) - 1] == CLEARSPACE)
-                    {
-                        (*x)--;
-                        return GARBAGE;
-                    }
-                    else if (map[*y][(*x) - 1] == WALL || map[*y][(*x) - 1] == BLOCK)
-                        return GARBAGE;
-                    else
-                    {
-                        (*x)--;
-                        f = flag(map, x, y, level, money, playName);
-                        (*x)++;
-                        return f;
-                    }
-                    break;
-                case RIGHT:
-                    if (map[*y][(*x) + 1] == CLEARSPACE)
-                    {
-                        (*x)++;
-                        return GARBAGE;
-                    }
-                    else if (map[*y][(*x) + 1] == WALL || map[*y][(*x) + 1] == BLOCK)
-                        return GARBAGE;
-                    else
-                    {
-                        (*x)++;
-                        f = flag(map, x, y, level, money, playName);
-                        (*x)--;
-                        return f;
-                    }
-                    break;
+            case UP:
+                if (map[(*y) - 1][*x] == CLEARSPACE)
+                {
+                    (*y)--;
+                    return GARBAGE;
+                }
+                else if (map[(*y) - 1][*x] == WALL || map[(*y) - 1][*x] == BLOCK) //벽
+                    return GARBAGE;
+                else
+                {
+                    (*y)--;
+                    f = flag(map, x, y, level, money, playName);
+                    (*y)++;
+                    return f;
+                }
+                break;
+            case DOWN:
+                if (map[(*y) + 1][*x] == CLEARSPACE)
+                {
+                    (*y)++;
+                    return GARBAGE;
+                }
+                else if (map[(*y) + 1][*x] == WALL || map[(*y) + 1][*x] == BLOCK)
+                    return GARBAGE;
+                else
+                {
+                    (*y)++;
+                    f = flag(map, x, y, level, money, playName);
+                    (*y)--;
+                    return f;
+                }
+                break;
+            case LEFT:
+                if (map[*y][(*x) - 1] == CLEARSPACE)
+                {
+                    (*x)--;
+                    return GARBAGE;
+                }
+                else if (map[*y][(*x) - 1] == WALL || map[*y][(*x) - 1] == BLOCK)
+                    return GARBAGE;
+                else
+                {
+                    (*x)--;
+                    f = flag(map, x, y, level, money, playName);
+                    (*x)++;
+                    return f;
+                }
+                break;
+            case RIGHT:
+                if (map[*y][(*x) + 1] == CLEARSPACE)
+                {
+                    (*x)++;
+                    return GARBAGE;
+                }
+                else if (map[*y][(*x) + 1] == WALL || map[*y][(*x) + 1] == BLOCK)
+                    return GARBAGE;
+                else
+                {
+                    (*x)++;
+                    f = flag(map, x, y, level, money, playName);
+                    (*x)--;
+                    return f;
+                }
+                break;
             }
         }
     }
@@ -651,6 +630,8 @@ int game_start(int(*map)[MAP_SIZE_W], int start, int* x, int* y, char playName[]
 {
     int item, level, ch;
     int money = 100; //초기 월급
+    int frame_cnt = 0;
+    int time_taken = 0;
 
     if (start == 0)
     {
@@ -667,6 +648,18 @@ int game_start(int(*map)[MAP_SIZE_W], int start, int* x, int* y, char playName[]
 
         while (1) {
             item = move_key(map, x, y, level, &money, playName);
+
+            frame_cnt++;
+            if (frame_cnt == 61) {   // TODO:not sure if it iterates 60 time and resetted
+                frame_cnt = 1;
+                time_taken++;
+            }
+            // how boss moves
+            if (frame_cnt % boss_speed_array[boss_speed_level] == 0) {
+                gotoxy(boss_x, boss_y);
+                draw_boss(map, y, x);
+            }
+            check_caught(y, x);
 
             //타이머
             gotoxy(61, 1);
@@ -710,12 +703,12 @@ int game_start(int(*map)[MAP_SIZE_W], int start, int* x, int* y, char playName[]
                 system("cls");
                 draw_map(map);
             }
-                //return -2: 메인메뉴로
+            //return -2: 메인메뉴로
             else if (item == -2)
             {
                 return 1;
             }
-                //return -3: 게임 종료
+            //return -3: 게임 종료
             else if (item == -3)
             {
                 return 0;
